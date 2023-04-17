94a2430f6c1b52a94d50738bc37e5bf8688ac1e9
Jina <jinayoussef@hotmail.co.uk>
2023-04-17 03:08:39 +0100

    added current highlights

--- weather-app/src/components/WeatherWidget/WeatherWidget.jsx
+++ weather-app/src/components/WeatherWidget/WeatherWidget.jsx
@@ -34,8 +34,43 @@ const WeatherWidget = (props) => {
 
         <div className='weather-widget__further-info'>
           <WeeklyWeather forecasts = {forecasts}/>
-          <HourlyWeather hourForecasts = {hourForecasts}/>
+          {/* <HourlyWeather hourForecasts = {hourForecasts}/> */}
 
+          <h3 className='weather-widget__current-highlights'>Current Highlights</h3>
+          <div className='weather-widget__now-info'>
+            <div className='weather-widget__info-card'>
+              <h5 className='weather-widget__info-title'>Feels Like</h5>
+              <h2 className='weather-widget__info-value'>{current.feelslike_c}°</h2>
+            </div>
+
+            <div className='weather-widget__info-card'>
+              <h5 className='weather-widget__info-title'>UV Index</h5>
+              <h2 className='weather-widget__info-value'>{current.uv}</h2>
+            </div>
+
+            <div className='weather-widget__info-card'>
+              <h5 className='weather-widget__info-title'>Sunrise & Sunset</h5>
+              <h2 className='weather-widget__info-value'>{forecast.forecastday[0].astro.sunrise}</h2>
+              <h2 className='weather-widget__info-value'>{forecast.forecastday[0].astro.sunset}</h2>
+            </div>
+
+            <div className='weather-widget__info-card'>
+              <h5 className='weather-widget__info-title'>Wind</h5>
+              <h2 className='weather-widget__info-value'>{current.wind_kph} km/h</h2>
+              <h2 className='weather-widget__info-value'>{current.wind_dir}</h2>
+            </div>
+
+            <div className='weather-widget__info-card'>
+              <h5 className='weather-widget__info-title'>Humidity</h5>
+              <h2 className='weather-widget__info-value'>{current.humidity} %</h2>
+            </div>
+
+            <div className='weather-widget__info-card'>
+              <h5 className='weather-widget__info-title'>Visibility</h5>
+              <h2 className='weather-widget__info-value'>{current.vis_km} km</h2>
+            </div>
+
+          </div>
           
         </div>
     </div>
